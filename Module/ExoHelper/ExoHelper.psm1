9452be45739afa45e454c9028a5771ed0b1243cb
Jiri Formacek <jiri.formacek@greycorbel.com>
2024-10-15 14:13:28 +0200

    feat: enhanced error reporting

--- Module/ExoHelper/ExoHelper.psm1
+++ Module/ExoHelper/ExoHelper.psm1
@@ -461,6 +461,10 @@ function Get-ExoException
                 return new-object ExoHelper.ExoException -ArgumentList @($httpCode, 'ExoErrorWithUnknownDetail', '', $message)
             }
         }
+        if($null -ne $errorRecord.error.innerError.internalException)
+        {
+            return new-object ExoHelper.ExoException -ArgumentList @($httpCode, 'ExoErrorWithInternalException', $errorRecord.error.innerError.type, $errorRecord.error.innerError.internalException.Message)
+        }
         if($null -ne $errorRecord.error)
         {
             return new-object ExoHelper.ExoException -ArgumentList @($httpCode, 'ExoErrorWithMissingDetail', '', "$($errorRecord.error)")
